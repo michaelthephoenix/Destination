8e9edc4046609442b8da4c7fcc7ab22705065b57
michael <nkono.mic@gmail.com>
2021-02-23 22:25:35 +0300

    conflict

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'views')));
+
+app.post('/post-feedback', function (req, res) {
+    dbConn.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('feedbacks').insertOne(req.body);
+    });    
+    res.send('Data received:\n' + JSON.stringify(req.body));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/view-feedbacks',  function(req, res) {
+    dbConn.then(function(db) {
+        db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
+            res.status(200).json(feedbacks);
+        });
+    });
 });
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
